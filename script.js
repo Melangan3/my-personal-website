6594ab5107b0cbb1a7df0d95066c532181e139fa
mark langan <markelangan@gmail.com>
2023-04-14 13:26:41 -0600

    image buttons linkedin

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Thanks for stopping by...";
 document.querySelector("body").appendChild(h2);
+const h1 = document.createElement("h1");
+function myFunction() {
+    var x = document.getElementById("container");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+       x.style.display = "none";
+      }
+}
